85f29ad5d95d26d1afe4fd2ffc721c1a4a5a43d6
Novar <xuwab77@gmail.com>
2024-09-20 02:48:26 +0800

    flow overview

---
+++ app/src/main/java/com/novar/supercorouutine/_4_flow/1_overview.kt
@@ -0,0 +1,11 @@
+package com.novar.supercorouutine._4_flow
+
+import kotlinx.coroutines.runBlocking
+
+fun main() = runBlocking<Unit> {
+    // StateFlow
+//  SharedFlow // event flow
+//  Flow // data flow
+//  Channel
+//  async {  }
+}
